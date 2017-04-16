69801555aa17d193a233e3401ab1f704942f94a5
jlhoward <jeff@thehowardfamily.net>
2017-04-16 16:35:34 -0400

    Updated UI with Table view

---
+++ FoodTracker-Ed/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker-Ed
+//
+//  Created by Jeff Howard on 4/16/17.
+//  Copyright © 2017 Jeff Howard. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
